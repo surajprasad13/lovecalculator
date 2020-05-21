9d8a9ec8bc670e7dd18af2436c3d6c484abe66b6
Suraj Prasad <48089919+surajprasad13@users.noreply.github.com>
2020-05-21 22:44:18 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var name2 = $("#name2").val();
 
     var url = "https://love-calculator.p.rapidapi.com/getPercentage?";
-
+    
     $.ajax({
       method: "GET",
       datatype: "json",
@@ -21,8 +21,10 @@ $(document).ready(function () {
       $(".result textarea").html(data["result"]);
       if (data["percentage"] < 50) {
         $("textarea").css("background-color", "red").css("color", "white");
+        $("i").css("color", "red").attr("class","fas fa-heart-broken");
       } else {
         $("textarea").css("background-color", "green").css("color", "white");
+        $("i").css("color", "green").attr("class", "fas fa-heartbeat");
       }
     });
 
